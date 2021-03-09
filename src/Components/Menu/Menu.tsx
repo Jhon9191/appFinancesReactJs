2294afb657220ea1b7eebb88baff9097a8c9f675
Jhon9191 <joaoantonio15161516@gmail.com>
2021-03-09 16:59:38 -0300

    Creating some menu items

--- src/Components/Menu/Menu.tsx
+++ src/Components/Menu/Menu.tsx
@@ -1,14 +1,23 @@
 import React, { useContext } from 'react';
 import styles from './Menu.module.css'
 import { Contexto } from '../../Context/context'
+import { VscDashboard } from 'react-icons/vsc'
+import { AiFillDollarCircle } from "react-icons/ai";
 
 const Menu = () => {
     const { variavel } = useContext(Contexto)
     return (
-            <div className={styles.menu} style={{ width: `${variavel}%` }}>
-                <h1>Oi</h1>
+        <div className={styles.menu} style={{ width: `${variavel}%` }}>
+            <div className={styles.menuItem}>
+                    <VscDashboard color="#FFF" size={30} />
+                    <small>Dashboard</small>
             </div>
-        
+            <div className={styles.menuItem}>
+                    <AiFillDollarCircle color="#FFF" size={30} />
+                    <small>BillyngCycles</small>
+            </div>
+        </div>
+
     );
 }
 
