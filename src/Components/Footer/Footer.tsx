8c126680b38f1d9fadd3c2224ee7103f572da417
Jhon9191 <joaoantonio15161516@gmail.com>
2021-03-10 06:41:19 -0300

    Adding copyright to the footer

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -3,6 +3,7 @@ import styles from '../Footer/Footer.module.css'
 const Footer = () => {
     return (
         <div className={styles.footer}>
+            <small>&copy;João Antônio 2021</small>
         </div>
     );
 }
