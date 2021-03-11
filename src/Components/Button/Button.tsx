f6884b4276df7fd753ab5b4eaefdaff48babb6ef
Jhon9191 <joaoantonio15161516@gmail.com>
2021-03-10 21:54:23 -0300

    Customizing buttons

--- src/Components/Button/Button.tsx
+++ src/Components/Button/Button.tsx
@@ -3,9 +3,24 @@ import styles from '../Button/Button.module.css'
 const Button = () =>{
     return(
         <div className={styles.container}>
-        <button className={styles.botao1}></button>
-        <button className={styles.botao2}></button>
-        <button className={styles.botao3}></button>
+        <button className={styles.botao1}>
+        <div>
+           <h2>R$ 10,00</h2>
+           <small>Total crédito</small>
+        </div>
+        </button>
+        <button className={styles.botao2}>
+        <div>
+           <h2>R$ 10,00</h2>
+           <small>Total débitos</small>
+        </div>
+        </button>
+        <button className={styles.botao3}>
+        <div>
+           <h2>R$ 20,00</h2>
+           <small>Valores calculados</small>
+        </div>
+        </button>
         </div>
     );
 }
